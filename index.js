adc563eb401d23325823e75e663ce80e31897437
eventEmitter <michael@joinbox.com>
2013-03-24 14:42:49 +0100

    extracted from iridium

---
+++ index.js
@@ -0,0 +1,56 @@
+
+	
+	var Class = require( "ee-class" );
+
+
+
+	module.exports = new Class( {
+
+		  __jobs: []
+		, __finishedCount: 0
+		, __callback: null
+
+
+		, init: function( callback ){
+			this.__callback = callback;
+		}
+
+
+		, add: function( job ){
+			this.__jobs.push( job );
+			return this;
+		}
+
+
+		, __complete: function(){
+			this.__finishedCount++;
+			if ( this.__finishedCount === this.__jobs.length ){
+				if ( this.__callback ) this.__callback();
+			}
+		}
+
+
+		, cancel: function( err ){
+			if ( this.__callback ) {
+				this.__callback( err );
+				delete this.__callback;
+			}
+		}
+
+		, start: function( callback ){
+			var i = this.__jobs.length;
+
+			this.__callback = callback;
+
+			if ( i > 0 ){
+				while( i-- ){
+					this.__jobs[ i ]( this.__complete.bind( this ) );
+				}
+			}
+			else {
+				if ( this.__callback ) this.__callback();
+			}
+		}
+	} );
+
+	
